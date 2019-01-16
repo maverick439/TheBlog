af766ad94c08928a1ef2310bf9fc9232bd94e2cc
maverick439 <srijan439@yahoo.com>
2019-01-16 10:06:54 +0530

    EditComments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,29 +6,61 @@ class CommentsController < ApplicationController
     @comment = @article.comments.order(created_at: :desc)
   end
 
-  def create
-    @comment = Comment.new(comment_params)
-    @article = Article.find(params[:article_id])
-  	@comment.article_id = params[:article_id]
-  	@comment.user_id = current_user.id
-  	@comment.save
+  # def new
+  #   @comment = Comment.new
+  # end
 
-  	respond_to do |format|
-  		format.html {redirect_to article_path(@comment.article)}
-  		format.js
-  	end
+  def create
+    @comment = Comment.new(:description => params[:comment][:description])
+    @article = Article.find(params[:article_id])
+    @comment.article_id = params[:article_id]
+    @comment.user_id = current_user.id
+    @comment.save
+
+    respond_to do |format|
+      format.html {redirect_to article_path(@comment.article)}
+      format.js
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @article = Article.find(params[:article_id])
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+
+  def update
+    @comment = Comment.find(params[:id])
+    @article = Article.find(params[:article_id])
+
+    if @comment.user_id == current_user.id
+      if @comment.update_attributes(:description => params[:comment][:description])
+        respond_to do |format|
+          format.html {redirect_to article_path(@comment.article)}
+          format.js
+        end
+      else
+        flash[:alert] = "Cannot Comment"
+        #render article_path
+      end
+    end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    #@comment = @article.comments.find(params[:id])
     if @comment.user_id == current_user.id
         @comment.destroy
     else
         flash[:notice] = "Cannot Delete"
     end
     respond_to do |format|
-      format.html {redirect_to article_path(@article)}
+      format.html {redirect_to article_path(@comment.article)}
       format.js
     end
   end
@@ -41,7 +73,7 @@ class CommentsController < ApplicationController
 
   private
 
- 	def comment_params
-  		params.require(:comment).permit(:description,:article_id,:user_id)
-	end
+  def comment_params
+      params.require(:comment).permit(:description,:article_id,:user_id)
+  end
 end
